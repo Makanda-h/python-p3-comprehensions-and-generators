0810484ed4164349746a35225403ebc7a01f2526
Hope <hopemwaura39@gmail.com>
2024-08-29 08:34:10 +0300

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [num for num in num_list if num % 2 == 0]
+    # for num in num_list:
+    #     if num % 2 == 0:
+    #         print(num)
+    #         return num_list
+        # else:
+        #     return []
     pass
 
 def make_exclamation(sentence_list):
+    return [sentence + '!' for sentence in sentence_list]
     pass
